b3c4bdc84a277b97efcf940e7d278aeff8d855de
keth42 <49443286+keth42@users.noreply.github.com>
2019-06-11 09:02:00 -0400

    h

--- Minesweeper/src/minesweeper/Cell.java
+++ Minesweeper/src/minesweeper/Cell.java
@@ -8,11 +8,15 @@ public class Cell {
     protected int x;
     protected int y;
     protected DConsole dc;
+    protected boolean covered;
+    protected Color col;
     
     public Cell(int x, int y, DConsole dc) {
         this.x = x;
         this.y = y;
         this.dc = dc;
+        this.covered = true;
+        this.col = new Color(128, 128, 128);
     }
     
     public int getX() {
@@ -24,8 +28,29 @@ public class Cell {
     }
     
     public void draw() {
-        dc.setPaint(new Color(128, 128, 128));
+        dc.setPaint(this.col);
         dc.fillRect(x, y, 20, 20);
     }
+    
+    public void setColor(int r, int g, int b) {
+        this.col = new Color(r, g, b);
+    }
+    
+    public boolean isCovered() {
+        return this.covered;
+    }
+    
+    public boolean isClicked() {
+        int mouseX = dc.getMouseXPosition();
+        int mouseY = dc.getMouseYPosition();
+        if(mouseX >= this.x - 10 && mouseX <= this.x + 10 && mouseY >= this.y - 10 && mouseY <= this.y + 10 && dc.getMouseButton(1)) {
+            return true;
+        }else {
+            return false;
+        }
+            
+    }
+    
+    
     
 }
--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -1,25 +1,32 @@
 package minesweeper;
 
 import DLibX.DConsole;
+import java.awt.Color;
 
 public class Minesweeper {
 
     public static void main(String[] args) {
-        
+
         DConsole dc = new DConsole();
         
-        
-        Cell[][] grid = new Cell[35][25];
-        
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                grid[i][j] = new Cell((i * 25) + 10, (j * 25) + 50, dc);
-                grid[i][j].draw();
+        dc.setOrigin(DConsole.ORIGIN_CENTER);
+
+        Cell[][] grid = new Cell[5][5];
+
+        while (true) {
+
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    grid[i][j] = new Cell((i * 25) + 10, (j * 25) + 50, dc);
+                    grid[i][j].draw();
+                    if(grid[i][j].isClicked()) {
+                        grid[i][j].setColor(255, 0, 128);
+                    }
+                }
             }
+
+            dc.redraw();
         }
-        
-        dc.redraw();
-        
     }
-    
+
 }
